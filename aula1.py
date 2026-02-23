fc796212ceabeda5c783a6a352d1a29ba3898dfb
Raquel Piel <deschk.pereira@escola.pr.gov.br>
2026-02-23 10:52:36 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,24 @@
+nome = "Raquel"
+idade = 16
+altura = 1.70
+genero = "feminino"
+cor_de_pele = "branca"
+cor_de_cabelo = "azul"
+tamanho_cabelo = "médio"
+estilo_cabelo = "liso"
+hobby = "jogar e dormir"
+
+# Novas variáveis
+curso = "Desenvolvimento de Sistemas"
+hobby_favorito = "escutar musica"
+
+# Autodescrição
+print("Olá, meu nome é", nome ,
+      ", tenho", idade, "anos e", altura, "m de altura.",
+      "Sou do gênero", genero ,
+      ", minha cor de pele é", cor_de_pele ,
+      ", meu cabelo é", cor_de_cabelo , 
+      ",", tamanho_cabelo, "e do estilo", estilo_cabelo , ".",
+      "Atualmente faço o curso de", curso ,
+      "e meu hobby é", hobby , 
+      ", sendo que meu hobby favorito é", hobby_favorito , ".")
